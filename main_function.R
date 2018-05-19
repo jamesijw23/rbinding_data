ee1a1a64b3b28e54f90fc9c516d2780e5ee286fc
Immanuel Williams <jamesijw23@gmail.com>
2018-05-19 13:05:09 -0700

    Function is completed with ability to combine df

--- main_function.R
+++ main_function.R
@@ -5,14 +5,6 @@ library(readxl)
 ## The folder must only contain files that are what you want to combine
 imp_files = list.files()[grep(".xlsx",list.files())]
 
-## You automatically should know the number of columns that you 
-## want the final dataframe to have
-number_columns = 3
-
-## Column Names need to be known before hand
-## This can be committed to be used by reading 1 file and observe
-c_names = c('id','password','interaction')
-
 
 ## Will have to change column that represents file
 
@@ -25,13 +17,8 @@ c_names = c('id','password','interaction')
 ## Output 2: Result of adding dataframe
 ####
 
-
+combine_file(imp_files){
 result_df = vector()
-
-
-
-
-
 for(iter_num in 1:length(imp_files)){
   
   if(iter_num == 1){
@@ -44,8 +31,10 @@ for(iter_num in 1:length(imp_files)){
     ######-----------------------------------------
     final_df$imp_var = rep(iter_num,nrow(final_df))
     
-    result_df[iter_num,1] = imp_files[iter_num]
+    ## c) Add result
     tmp_result = c(imp_files[iter_num],'Success')
+    
+    col_name_check = T; col_number_check = T
     
   } else {
     
@@ -58,13 +47,13 @@ for(iter_num in 1:length(imp_files)){
     ######-----------------------------------------
     ## b) Check Dimensions of new dataframes
     ######-----------------------------------------
-    col_number_check = ncol(data_tmp) == ncol(final_df)
+    col_number_check = ncol(data_tmp) == (ncol(final_df) -1)
     
     ######-----------------------------------------
     ## c) Check if columns are the same after first df
     ######-----------------------------------------
     col_name_check = identical(colnames(data_tmp),
-                               colnames(final_df)[-nrow(final_df)])
+                               colnames(final_df)[-ncol(final_df)])
     ######-----------------------------------------
     ## d) Add column based on date to both data sets
     ######-----------------------------------------
@@ -85,10 +74,10 @@ for(iter_num in 1:length(imp_files)){
     
   } else if (iter_num > 1 &  col_name_check == T & col_number_check == F){
     tmp_result <- c(imp_files[iter_num],'Wrong # of Columns')
-  } else {
+  } else if(iter_num >1){
     tmp_result <- c(imp_files[iter_num],'Wrong # of Columns and Wrong Column Names')
   }
   result_df = rbind(result_df,tmp_result)
 }
-
-
+return(list(f_df = final_df,status = result_df))
+}
