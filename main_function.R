4cb1f480bf47b748c62e9b17824b4e03505d8ac2
Immanuel Williams <jamesijw23@gmail.com>
2018-05-19 12:15:00 -0700

    First df will be the template

--- main_function.R
+++ main_function.R
@@ -4,14 +4,14 @@ library(readxl)
 
 ## The folder must only contain files that are what you want to combine
 imp_files = list.files()[grep(".xlsx",list.files())]
+
 ## You automatically should know the number of columns that you 
 ## want the final dataframe to have
+number_columns = 3
 
-
-
-
-
-
+## Column Names need to be known before hand
+## This can be committed to be used by reading 1 file and observe
+c_names = c('id','password','interaction')
 
 ##-------------------------------------------
 
@@ -19,18 +19,19 @@ imp_files = list.files()[grep(".xlsx",list.files())]
 ## Function
 ## Input 1: List of files that should be into dataframe 
 ## Input 2: Number of columns of final dataframe
+## Input 3: Columns are already known
 ## Output 1: Combined dataframe
 ## Output 2: Result of adding dataframe
 ####
 
 
-## Empty dfs for output
-final_df = data.frame(id=numeric(),
-                      password=character(),
-                      interaction=character())
-
 result_df = data.frame(df_name=character(),
                        result=character())
+
+
+
+
+
 
 
 
@@ -44,18 +45,21 @@ data_tmp = read_excel(imp_files[1])
 ######-----------------------------------------
 ## b) Check Dimensions of new dataframes
 ######-----------------------------------------
-(dim_0227 = dim(data_20170227))
+col_num_check = ncol(data_tmp) == number_columns
+
 
 ######-----------------------------------------
-## c) Check if columns are the same
+## c) Check if columns are the same after first df
 ######-----------------------------------------
-(check_31 = identical(colnames(data_20170227),
-                      colnames(data_until_0213)[-34]))
+col_name_check = identical(colnames(data_tmp),
+                      colnames(final_df)[-nrow(final_df)])
+
+
 
 ######-----------------------------------------
 ## d) Add column based on date to both data sets
 ######-----------------------------------------
-data_20170227$date = rep('02-27-2017',nrow(data_20170227))
+data_tmp$imp_var = rep('02-27-2017',nrow(data__tmp))
 
 ######-----------------------------------------
 ## e) Check if columns are the same
